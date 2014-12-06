c25f302acddbd17479d898cdfeca643669ed70c4
himalie <himalie1@gmail.com>
2014-12-06 08:58:29 +0530

    fetching data from json object

--- www/js/user-controllers.js
+++ www/js/user-controllers.js
@@ -6,13 +6,9 @@ angular.module('starter.controllers')
 
       $http.get('http://localhost/api/user').
           success(function(data, status, headers, config) {
-          	// learning how to access data returned from the web service
-            console.log(data[0]);
-            console.log(data[0].Vehicles[0].vehicle_no);
-            $scope.users.push(data);
             angular.forEach(data, function(data) {
-            	console.log(" first name : " + data.first_name);
-            	// console.log(" first name : " + data.Vehicles[0].vehicle_no);
+              $scope.users.push(data);
+            	console.log(" first name : " + data.first_name.trim());
           	})
         });
 })
