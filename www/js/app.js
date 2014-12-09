fbe8c26cbe72bb6ac87ecbce6ff053e2e6bea169
himalie <himalie1@gmail.com>
2014-12-09 19:27:53 +0530

    added code to solve the
    Access-Control-Allow-Origin problem .. just to test

--- www/js/app.js
+++ www/js/app.js
@@ -24,6 +24,10 @@ angular.module('starter', ['ionic', 'starter.controllers'])
 
 .config(function($stateProvider, $urlRouterProvider, $httpProvider) {
   $httpProvider.defaults.useXDomain = true;﻿
+  delete $httpProvider.defaults.headers.common["X-Requested-With"];
+  $httpProvider.defaults.headers.common["Accept"] = "application/json";
+  $httpProvider.defaults.headers.common["Content-Type"] = "application/json";
+
   $stateProvider
 
     .state('app', {
