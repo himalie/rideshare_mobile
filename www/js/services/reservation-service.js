af08d9779607cfc289798e1ba2ebdfd4ade670e3
himalie <himalie1@gmail.com>
2014-12-22 17:02:44 +0530

    code cleanup and comments

--- www/js/services/reservation-service.js
+++ www/js/services/reservation-service.js
@@ -3,46 +3,36 @@ angular.module('starter')
 
 .factory('Reservation', ['$http', '$rootScope', '$q', 'UserFactory', 'RideFactory', function($http, $rootScope, $q, UserFactory, RideFactory) {
 
-
-var urlBase = 'http://localhost/ARideShare/api/riderinfo';
-var Reservation = {};
-console.log('wwwwwwwwddddddddddddddddddddwwwwwwwwwww')
+    var urlBase = 'http://localhost/ARideShare/api/riderinfo';
+    var Reservation = {};
 
 
-Reservation.joinRide = function(position) {
-    console.log('wwwwwwwwwwwwwwwwwwwwwwwwwwwwww')
-    console.log(position)
-	if (UserFactory.signedIn()) { 
-        return $http.post(urlBase, {
-                user_id : UserFactory.currentUser.user_id,
-                ride_id : RideFactory.currentRide.ride_id,
-                status : 'Joined',
-                start_latitude : position.latitude,
-                start_longitude : position.longitude
+    Reservation.joinRide = function(position) {
+    	if (UserFactory.signedIn()) { 
+            return $http.post(urlBase, {
+                    user_id : UserFactory.currentUser.user_id,
+                    ride_id : RideFactory.currentRide.ride_id,
+                    status : 'Joined',
+                    start_latitude : position.latitude,
+                    start_longitude : position.longitude
 
-            }).
-              success(function(data, status, headers, config) {
-                // this callback will be called asynchronously
-                // when the response is available
-                console.log('successsssssssssssssssssss')
-                Reservation.currentRes = data;
-              }).
-              error(function (data, status, headers, config) {
-                Reservation.currentRes = null;
+                }).
+                  success(function(data, status, headers, config) {
+                    // this callback will be called asynchronously
+                    // when the response is available
+                    Reservation.currentRes = data;
+                  }).
+                  error(function (data, status, headers, config) {
+                    Reservation.currentRes = null;
 
-            });
-        }
+                });
+            }
 
-    };
+        };
 
+    // a passenger decides to leave a particular ride
     Reservation.leaveRide = function (ride_id_, user_id_){
-
-
-        //http://localhost/ARideShare/api/riderinfo?ride_id=23&user_id=1
-        
         return $http.delete(urlBase, {params: {ride_id : ride_id_, user_id : user_id_}});
-
     };
-
     return Reservation;
 }])
