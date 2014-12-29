ee25536df828f8a62cd3907e5a65993b7c2ce81f
himalie <himalie1@gmail.com>
2014-12-29 23:23:50 +0530

    filter modification

--- www/js/filters/remove-last-node.js
+++ www/js/filters/remove-last-node.js
@@ -1,7 +1,8 @@
 angular.module('starter')
     .filter('removeLastNode', function() {
-        return function(input) {
-            var arr = input.split(',').pop();
+        return function(input) {        	
+            var arr = input.split(',').slice(0, -1);
+            console.log(arr.join(", "));
             return arr.join(", ");
         }
     });
