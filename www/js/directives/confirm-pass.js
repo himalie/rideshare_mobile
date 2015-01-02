00431c263e50d26f9f56aa41c67b254bee662398
Dragfreeze <Dragfreeze@gmail.com>
2015-01-02 21:59:44 +0530

    password confirmation

---
+++ www/js/directives/confirm-pass.js
@@ -0,0 +1,18 @@
+angular.module('starter').directive("confirmPass", function() {
+    return {
+        require: "ngModel",
+        scope: {
+            otherModelValue: "=compareTo"
+        },
+        link: function(scope, element, attributes, ngModel) {
+
+            ngModel.$validators.compareTo = function(modelValue) {
+                return modelValue == scope.otherModelValue;
+            };
+
+            scope.$watch("otherModelValue", function() {
+                ngModel.$validate();
+            });
+        }
+    };
+});
